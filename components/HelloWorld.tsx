177d1946b5faf1389b3ef7b9c036f97d765ad580
fauzanalhfzh <zenbangga@gmail.com>
2024-12-16 13:56:20 +0700

    first commit

---
+++ components/HelloWorld.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HelloWorld = () => {
+  return (
+    <div>
+        <h1 className='title text-2xl'>Hello World</h1>
+        <p>Selamat belajar Next JS</p>
+    </div>
+  )
+}
+
+export default HelloWorld
